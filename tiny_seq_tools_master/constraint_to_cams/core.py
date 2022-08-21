f768f8ac415fcec0b7025e3ce47a5f30ef321d41
NickTiny <nick@tinymedia.ca>
2022-08-20 23:16:34 -0400

    [constraint to camera] add docstring parameter and return type

--- tiny_seq_tools_master/constraint_to_cams/core.py
+++ tiny_seq_tools_master/constraint_to_cams/core.py
@@ -3,9 +3,14 @@ import bpy
 
 def constraints_to_active_camera(
     strip: bpy.types.Sequence,
-):
+) -> bool:
+    """Set Constraint target to active trip's camera"""
+
+    rot_to_seq_cam_status = False
     for item in strip.id_data.rot_to_seq_cam_items:
         obj = item.object
         for constraint in obj.constraints:
             if constraint.type == "COPY_ROTATION":
                 constraint.target = strip.scene_camera
+                rot_to_seq_cam_status |= True
+    return rot_to_seq_cam_status
