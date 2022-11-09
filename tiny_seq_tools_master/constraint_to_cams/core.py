5544004743facb2b8f5f0706e5bef082922bb5ea
NickTiny <nick@tinymedia.ca>
2022-11-09 16:10:44 -0500

    Change Rotate to Strip Camera  - Rotate to camera based on constraint name  - Refresh List on Load

--- tiny_seq_tools_master/constraint_to_cams/core.py
+++ tiny_seq_tools_master/constraint_to_cams/core.py
@@ -12,5 +12,43 @@ def constraints_to_active_camera(
         for constraint in obj.constraints:
             if constraint.type == "COPY_ROTATION":
                 constraint.target = strip.scene_camera
+                constraint.name = "ROT_TO_SEQ_CAM"
                 rot_to_seq_cam_status |= True
     return rot_to_seq_cam_status
+
+
+def check_rot_to_cam_status(obj):
+    for constraint in obj.constraints:
+        if constraint.type == "COPY_ROTATION" and constraint.name == "ROT_TO_SEQ_CAM":
+            return True
+    else:
+        return False
+
+
+def set_rot_to_seq_cam(obj):
+    const = obj.constraints.new("COPY_ROTATION")
+    const.name = "ROT_TO_SEQ_CAM"
+    const.use_x = False
+    const.use_y = False
+    const.use_z = True
+    return const
+
+
+def refresh_rot_to_cam_list(context, strip):
+    rot_to_seq_cam_items = context.window_manager.rot_to_seq_cam_items
+
+    # Clear seq cam list
+    rot_to_seq_cam_items.clear()
+
+    # Check for active strip type
+    if not strip or strip.type != "SCENE":
+        return
+
+    # Build seq cam list
+    for obj in strip.scene.objects:
+        if obj not in [x.object for x in rot_to_seq_cam_items]:
+            status = check_rot_to_cam_status(obj)
+            if status is True:
+                add_rot_to_cam_item = rot_to_seq_cam_items.add()
+                add_rot_to_cam_item.object = obj
+    return True
--- tiny_seq_tools_master/core/__init__.py
+++ tiny_seq_tools_master/core/__init__.py
@@ -1,11 +1,13 @@
-from tiny_seq_tools_master.core import save_pre, sync_core
+from tiny_seq_tools_master.core import save_pre, sync_core, load_post
 
 
 def register():
     sync_core.register()
     save_pre.register()
+    load_post.register()
 
 
 def unregister():
     sync_core.unregister()
     save_pre.unregister()
+    load_post.unregister()
