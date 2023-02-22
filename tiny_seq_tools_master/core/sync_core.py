5da02c5da743b86c97d6111d9fda6c907779db81
NickTiny <nick@tinymedia.ca>
2023-02-22 10:28:44 -0500

    fix rotate_to_strip_during_render
     - Use last_master_strip instead of get_master_strip
    - Skip if already updated

--- tiny_seq_tools_master/core/sync_core.py
+++ tiny_seq_tools_master/core/sync_core.py
@@ -3,12 +3,33 @@ import bpy
 from tiny_seq_tools_master.constraint_to_cams.core import constraints_to_active_camera
 from tiny_seq_tools_master.line_art_tools.core import sync_strip_camera_to_seq_line_art
 
+from operator import attrgetter
+
+OldStrip = ""
+
+
+
+def find_scene_strip(edit_scene):
+    seq = [strip for strip in edit_scene.sequence_editor.sequences if strip.type == "SCENE"]
+    seq = sorted(seq, key=attrgetter("channel", "frame_final_start"))
+    for strip in seq:
+        current_keyframe = edit_scene.frame_current
+        #current_keyframe = int(edit_scene.frame_current - strip.frame_start + strip.scene.frame_start)
+        if strip.frame_final_start <= current_keyframe and strip.frame_final_end > current_keyframe:
+            #OldStrip = strip.name
+            return strip
+
 def update_constraint_camera(scene):
+    global OldStrip 
     try:
-        from spa_sequencer.sync.core import (get_sync_master_strip)
-        strip, _= get_sync_master_strip()
+        from spa_sequencer.sync.core import (get_sync_settings)
+        master_scene = get_sync_settings().master_scene
+        strip = master_scene.sequence_editor.sequences[get_sync_settings().last_master_strip]
+        if strip.name == OldStrip:
+            return
         constraints_to_active_camera(strip)
-        sync_strip_camera_to_seq_line_art(strip)   
+        sync_strip_camera_to_seq_line_art(strip)
+        OldStrip == strip.name
     except ModuleNotFoundError:
         return
 
