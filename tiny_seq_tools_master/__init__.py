c81ed9d458b22abda1b8eab0b425b8714114b9f1
TinyNick <nick@blender.org>
2023-03-23 19:33:35 -0400

    Version Bump 1.0.9

--- tiny_seq_tools_master/__init__.py
+++ tiny_seq_tools_master/__init__.py
@@ -12,7 +12,7 @@ bl_info = {
     "author": "Nick Alberelli ",
     "description": "Tools for creating cut-out animation with Tiny's BLOWN APART Characters and Backgrounds",
     "blender": (3, 2, 0),
-    "version": (1, 0, 8),
+    "version": (1, 0, 9),
     "location": "Sequencer > Sidebar > Tiny Sequencer Tools",
     "warning": "",
     "category": "Sequencer",
